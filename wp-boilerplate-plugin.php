f80358151ff80f5ceea386d5ff049d346d2fd837
Denis Žoljom <denis.zoljom@gmail.com>
2019-02-06 15:53:33 +0100

    Minor improvements
    Add uninstall file and fixed autoloading

--- wp-boilerplate-plugin.php
+++ wp-boilerplate-plugin.php
@@ -18,55 +18,58 @@
  * Requires PHP:      7.2
  */
 
-namespace WP_Boilerplate_Plugin {
+namespace WP_Boilerplate_Plugin;
 
-  use WP_Boilerplate_Plugin\Core\Plugin_Factory;
+use WP_Boilerplate_Plugin\Core\Plugin_Factory;
 
-  /*
-   * Make sure this file is only run from within WordPress.
-   */
-  defined( 'ABSPATH' ) || die();
+/*
+ * Make sure this file is only run from within WordPress.
+ */
+defined( 'ABSPATH' ) || die();
 
-  /**
-   * Include the autoloader so we can dynamically include the rest of the classes.
-   *
-   * @since 1.0.0
-   */
-  require_once __DIR__ . '/vendor/autoload.php';
+/**
+ * Include the autoloader so we can dynamically include the rest of the classes.
+ *
+ * @since 1.0.0
+ */
+$autoloader = __DIR__ . '/vendor/autoload.php';
 
-  /**
-   * Plugin URL const
-   */
-  define( 'PLUGIN_BASE_URL', plugin_dir_url( __FILE__ ) );
-
-  /**
-   * The code that runs during plugin activation.
-   *
-   * @since 1.0.0
-   */
-  register_activation_hook(
-    __FILE__,
-    function() {
-      Plugin_Factory::create()->activate();
-    }
-  );
-
-  /**
-   * The code that runs during plugin deactivation.
-   *
-   * @since 1.0.0
-   */
-  register_deactivation_hook(
-    __FILE__,
-    function() {
-      Plugin_Factory::create()->deactivate();
-    }
-  );
-
-  /**
-   * Begins plugin execution.
-   *
-   * @since 1.0.0
-   */
-  Plugin_Factory::create()->register();
+if ( is_readable( $autoloader ) ) {
+  require_once $autoloader;
 }
+
+/**
+ * Plugin URL const
+ */
+define( 'PLUGIN_BASE_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * The code that runs during plugin activation.
+ *
+ * @since 1.0.0
+ */
+register_activation_hook(
+  __FILE__,
+  function() {
+    Plugin_Factory::create()->activate();
+  }
+);
+
+/**
+ * The code that runs during plugin deactivation.
+ *
+ * @since 1.0.0
+ */
+register_deactivation_hook(
+  __FILE__,
+  function() {
+    Plugin_Factory::create()->deactivate();
+  }
+);
+
+/**
+ * Begins plugin execution.
+ *
+ * @since 1.0.0
+ */
+Plugin_Factory::create()->register();
