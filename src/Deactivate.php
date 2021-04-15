5a45bd1946468a7a7d50a4376378466c68a92bd1
Ivan Grginov <ivan.grginov@infinum.com>
2021-04-15 10:25:59 +0200

    Fix plugin activation deactivation

---
+++ src/Deactivate.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The file that defines actions on plugin deactivation.
+ *
+ * @package EightshiftBoilerplate
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftBoilerplate;
+
+use EightshiftBoilerplateVendor\EightshiftLibs\Plugin\HasDeactivationInterface;
+
+/**
+ * The plugin deactivation class.
+ */
+class Deactivate implements HasDeactivationInterface
+{
+
+	/**
+	 * Deactivate the plugin.
+	 */
+	public function deactivate(): void
+	{
+
+		\flush_rewrite_rules();
+	}
+}
--- uninstall.php
+++ uninstall.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
  * Fired when the plugin is uninstalled.
  *
- * @since 1.0.0
- *
- * @package Eightshift_Boilerplate
+ * @package EightshiftBoilerplate
  */
 
-declare( strict_types=1 );
+declare(strict_types=1);
 
-if ( ! current_user_can( 'activate_plugins' ) ) {
-  return;
+if (! current_user_can('activate_plugins')) {
+	return;
 }
 
 // If uninstall is not called from WordPress, then exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-  exit;
+if (! defined('WP_UNINSTALL_PLUGIN')) {
+	exit;
 }
